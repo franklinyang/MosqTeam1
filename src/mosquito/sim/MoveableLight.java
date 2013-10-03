d6815571a0e51221f7757087a8950d5f39ff37c4
Timothy Miller <timothymiller@timmer.local>
2013-10-03 02:31:53 -0400

    Fixed moveDiag and added goTo

--- src/mosquito/sim/MoveableLight.java
+++ src/mosquito/sim/MoveableLight.java
@@ -5,7 +5,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class MoveableLight extends Light {
-
+	public enum Corner {
+		NW, NE, SE, SW
+	}
+	
 	public MoveableLight(double x, double y, double d, double t, double s) {
 		super(x, y, d, t, s);
 	}
@@ -78,42 +81,38 @@ public class MoveableLight extends Light {
 		return false;
 	}
 	
-	public boolean moveDiag(Line2D wall, boolean up) {
+	public boolean moveDiag(Line2D wall, Corner corner) {
 		//This function takes the coordinates of the line that defines the 
 		//diagonal slope it should traverse as its first argument
 		//up is true if it is traversing upwards and false if it is traversing
 		//downwards
 		double xDiff = wall.getX1()-wall.getX2();
 		double yDiff = wall.getY1()-wall.getY2();
-		boolean yDiffIsPositive = (yDiff > 0);
 		double dist = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
 		double cosT = xDiff/dist;
 		double sinT = yDiff/dist;
 		double xMove = Math.pow(cosT, 2);
 		double yMove = Math.pow(sinT, 2);
-		if (up) { 
-			if (isLegalMove(this.x+xMove, this.y+yMove)) {
-				if (yDiffIsPositive) {
-					this.x+=xMove;
-					this.y+=yMove;
-				} else {
-					this.x-=xMove;
-					this.y-=yMove;
-				}
-				return true;
-			} else return false;
-		} else { 
-			if (isLegalMove(this.x+xMove, this.y+yMove)) {
-				if (!yDiffIsPositive) {
-					this.x+=xMove;
-					this.y+=yMove;
-				} else {
-					this.x-=xMove;
-					this.y-=yMove;
-				}
-				return true;
-			} else return false;
-		} 
+		if (corner == Corner.NW || corner == Corner.NE) this.y-=yMove;
+		else this.y+=yMove;
+		if (corner == Corner.NE || corner == Corner.SE) this.x+=xMove;
+		else this.x-=xMove;
+		return true;
+	}
+	
+	public boolean goTo(double x, double y) {
+		//This function goes directly to the point (x,y)
+		Line2D line = new Line2D.Double(this.x, this.y, x, y);
+		Corner corner;
+		if (this.y>y) {
+			if (this.x<x) corner = Corner.NE;
+			else corner = Corner.NW;
+		} else {
+			if (this.x<x) corner = Corner.SE;
+			else corner = Corner.SW;
+		}
+		moveDiag(line, corner);
+		return false;
 	}
 	
 	protected boolean isLegalMove(double newX, double newY) {
